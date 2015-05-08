cdb4b6f2e3a69ec79e6c020f0e92447a7cbffbfa
Michael Vostrikov <michael.vostrikov@gmail.com>
2015-05-08 19:13:30 +0500

    Button name

--- views/work-order-type/index.php
+++ views/work-order-type/index.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Work Order Type', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Add', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([
